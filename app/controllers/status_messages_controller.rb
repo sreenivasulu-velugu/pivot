f8f3b46c0f18248b1ae5897ba29bbe003f6869b3
shyam mohan kanojia <shyammohankanojia@gmail.com>
2012-04-09 00:50:37 +0530

    comment model fixes

--- app/controllers/status_messages_controller.rb
+++
@@ -1,9 +0,0 @@
-#   Copyright (c) 2010-2011, Diaspora Inc.  This file is
-#   licensed under the Affero General Public License version 3 or later.  See
-#   the COPYRIGHT file.
-
-class StatusMessagesController < ApplicationController
-  before_filter :authenticate_user!
-
-  
-end
