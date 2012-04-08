2564cd9d8384b4e4732abd2cbf385950ee65a5ff
shyam mohan kanojia <shyammohankanojia@gmail.com>
2012-04-09 00:52:58 +0530

    comment model fixes

--- app/mailers/notification_mailers/confirm_email.rb
+++
@@ -1,8 +0,0 @@
-module NotificationMailers
-  class ConfirmEmail < NotificationMailers::Base
-    def set_headers
-      @headers[:to] = name_and_address(@recipient.profile.first_name, @recipient.unconfirmed_email)
-      @headers[:subject] = I18n.t('notifier.confirm_email.subject', :unconfirmed_email => @recipient.unconfirmed_email)
-    end
-  end
-end
